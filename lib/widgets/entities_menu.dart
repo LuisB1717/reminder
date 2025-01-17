2b5d700bd6d695ea0d4ed7d4aa49e364f3c5c268
@mariovazquez-4 <mavcu44hang@gmail.com>
2025-01-17 16:20:18 -0500

    padding inside container

--- lib/widgets/entities_menu.dart
+++ lib/widgets/entities_menu.dart
@@ -15,21 +15,22 @@ class EntitiesMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
       padding: const EdgeInsets.symmetric(
-        horizontal: 120,
-        vertical: 12,
+        horizontal: 120.0,
+        vertical: 10.0,
       ),
-      child: Container(
-        height: 60,
-        decoration: decoration,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildMenuItem(Icons.person_outline, 0),
-            _buildMenuItem(Icons.business_outlined, 1),
-          ],
-        ),
+      height: 60,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildMenuItem(Icons.person_outline, 0),
+          _buildMenuItem(Icons.business_outlined, 1),
+        ],
       ),
     );
   }
@@ -41,8 +42,9 @@ class EntitiesMenu extends StatelessWidget {
         width: 40,
         height: 40,
         decoration: BoxDecoration(
-          color:
-              selectedIndex == index ? AppColors.box : AppColors.transparency,
+          color: selectedIndex == index
+              ? AppColors.bgButton
+              : AppColors.transparent,
           shape: BoxShape.circle,
         ),
         child: Icon(
