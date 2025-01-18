1d891fb3bf36c4a9ca54f5ff0792138910666e30
LuisB1717 <luisbarboz17@gmail.com>
2025-01-18 18:02:01 -0500

    Created widget filter button y Modify district parameter id in town

--- lib/api/town.dart
+++ lib/api/town.dart
@@ -1,7 +1,7 @@
 import 'package:reminder/core/town.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-Future<List<Town>> getTowns(int id) async {
+Future<List<Town>> getTowns(String id) async {
   final supabase = Supabase.instance.client;
   final rows = await supabase.from('town').select().eq('district_id', id);
   final towns = rows.map(Town.from).toList();
